325c203cd14f64bf7b98b31242ee98c475364bba
KamiDummy <pin.inci@hotmail.com>
2025-01-24 09:25:52 -0500

    update  to dice game

--- lab03.py
+++ lab03.py
@@ -1,2 +1,8 @@
 # dice options
 diceOptions  = list(range(1, 7))
+
+#Wepons array 
+weapons = ["fist", "knife", "Club", "Gun", "Bomb", "Nuclear Bomb"]
+
+# Diplsay available weapons 
+print ("Available weapons: ", ', '.join(weapons))
