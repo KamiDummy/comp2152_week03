79ba9fbb59f70e7216cf02c0167b000e00644ad1
KamiDummy <pin.inci@hotmail.com>
2025-01-24 09:28:47 -0500

    update  to dice game

--- lab03.py
+++ lab03.py
@@ -6,3 +6,15 @@ weapons = ["fist", "knife", "Club", "Gun", "Bomb", "Nuclear Bomb"]
 
 # Diplsay available weapons 
 print ("Available weapons: ", ', '.join(weapons))
+
+def  get_combat_strength(prompt) {
+    while True:
+        try:
+            value = int(input(prompt))
+            if 1 <= value <= 6:
+                return value
+            else: 
+                print ("value invalid, me wants 1-6")
+        except ValueError:
+            print("Invalid input")
+}
